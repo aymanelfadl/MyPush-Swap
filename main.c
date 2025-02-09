dea3062e07903cec42bbd9b49b38b2aeeabddeaa
ayman elfadl <aymanelfadl16@gmail.com>
2025-02-09 21:43:51 +0100

    need parsin g now

--- main.c
+++ main.c
@@ -11,31 +11,49 @@ void print_stack(t_stack *stack)
     printf("NULL\n");
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    t_stack *stack_a = NULL;
-    t_stack *stack_b = NULL;
+    int i;
+    char **args;
 
-    add_back(&stack_a, new_node(1));
-    add_back(&stack_a, new_node(2));
-    add_back(&stack_a, new_node(3));
-    add_back(&stack_a, new_node(4));
-
-    add_back(&stack_b, new_node(1));
-    add_back(&stack_b, new_node(2));
-    add_back(&stack_b, new_node(3));
-    add_back(&stack_b, new_node(4));
-
-    printf("Before {A}:\n");
-    print_stack(stack_a);
-    
-    sa(&stack_a, 1);
-
-    printf("After {A}:\n");
-    print_stack(stack_a);
-    
-    free_stack(stack_b);
-    free_stack(stack_a);
-
-    return 0;
+    i = 1;
+    if (argc == 1)
+        return 0;
+    while (i < argc)
+    {
+        args = ft_split((const char *)argv[i], ' ');
+        
+    }
 }
+
+
+
+
+// int main(void)
+// {
+//     t_stack *stack_a = NULL;
+//     t_stack *stack_b = NULL;
+
+//     add_back(&stack_a, new_node(1));
+//     add_back(&stack_a, new_node(2));
+//     add_back(&stack_a, new_node(3));
+//     add_back(&stack_a, new_node(4));
+
+//     add_back(&stack_b, new_node(1));
+//     add_back(&stack_b, new_node(2));
+//     add_back(&stack_b, new_node(3));
+//     add_back(&stack_b, new_node(4));
+
+//     printf("Before {A}:\n");
+//     print_stack(stack_a);
+    
+//     sa(&stack_a, 1);
+
+//     printf("After {A}:\n");
+//     print_stack(stack_a);
+    
+//     free_stack(stack_b);
+//     free_stack(stack_a);
+
+//     return 0;
+// }
